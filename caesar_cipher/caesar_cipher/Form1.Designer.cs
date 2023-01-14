dcc74f57ea40890befbb43c7270bd7ce328994e1
joxerx <n.lebedinsky@bk.ru>
2023-01-14 08:43:24 +0300

    Add files via upload

---
+++ caesar_cipher/caesar_cipher/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace caesar_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toCrypt = new System.Windows.Forms.GroupBox();
+            this.mover = new System.Windows.Forms.TextBox();
+            this.cipher = new System.Windows.Forms.Button();
+            this.encrypted_text = new System.Windows.Forms.TextBox();
+            this.origin_text = new System.Windows.Forms.TextBox();
+            this.decryptor = new System.Windows.Forms.Button();
+            this.resultAnlasyzis = new System.Windows.Forms.Label();
+            this.toCrypt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toCrypt
+            // 
+            this.toCrypt.Controls.Add(this.resultAnlasyzis);
+            this.toCrypt.Controls.Add(this.decryptor);
+            this.toCrypt.Controls.Add(this.mover);
+            this.toCrypt.Controls.Add(this.cipher);
+            this.toCrypt.Controls.Add(this.encrypted_text);
+            this.toCrypt.Controls.Add(this.origin_text);
+            this.toCrypt.Location = new System.Drawing.Point(13, 13);
+            this.toCrypt.Name = "toCrypt";
+            this.toCrypt.Size = new System.Drawing.Size(567, 301);
+            this.toCrypt.TabIndex = 0;
+            this.toCrypt.TabStop = false;
+            this.toCrypt.Text = "Шифрование";
+            // 
+            // mover
+            // 
+            this.mover.Location = new System.Drawing.Point(338, 147);
+            this.mover.Name = "mover";
+            this.mover.Size = new System.Drawing.Size(100, 20);
+            this.mover.TabIndex = 3;
+            // 
+            // cipher
+            // 
+            this.cipher.Location = new System.Drawing.Point(444, 144);
+            this.cipher.Name = "cipher";
+            this.cipher.Size = new System.Drawing.Size(117, 23);
+            this.cipher.TabIndex = 2;
+            this.cipher.Text = "Зашифровать";
+            this.cipher.UseVisualStyleBackColor = true;
+            this.cipher.Click += new System.EventHandler(this.cipher_Click);
+            // 
+            // encrypted_text
+            // 
+            this.encrypted_text.Location = new System.Drawing.Point(7, 173);
+            this.encrypted_text.Multiline = true;
+            this.encrypted_text.Name = "encrypted_text";
+            this.encrypted_text.Size = new System.Drawing.Size(554, 118);
+            this.encrypted_text.TabIndex = 1;
+            // 
+            // origin_text
+            // 
+            this.origin_text.Location = new System.Drawing.Point(7, 20);
+            this.origin_text.Multiline = true;
+            this.origin_text.Name = "origin_text";
+            this.origin_text.Size = new System.Drawing.Size(554, 118);
+            this.origin_text.TabIndex = 0;
+            // 
+            // decryptor
+            // 
+            this.decryptor.Location = new System.Drawing.Point(6, 147);
+            this.decryptor.Name = "decryptor";
+            this.decryptor.Size = new System.Drawing.Size(163, 23);
+            this.decryptor.TabIndex = 4;
+            this.decryptor.Text = "Вычислить смещение";
+            this.decryptor.UseVisualStyleBackColor = true;
+            this.decryptor.Click += new System.EventHandler(this.decryptor_Click);
+            // 
+            // resultAnlasyzis
+            // 
+            this.resultAnlasyzis.AutoSize = true;
+            this.resultAnlasyzis.Location = new System.Drawing.Point(176, 154);
+            this.resultAnlasyzis.Name = "resultAnlasyzis";
+            this.resultAnlasyzis.Size = new System.Drawing.Size(0, 13);
+            this.resultAnlasyzis.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 329);
+            this.Controls.Add(this.toCrypt);
+            this.Name = "Form1";
+            this.Text = "Шифр Цезаря";
+            this.toCrypt.ResumeLayout(false);
+            this.toCrypt.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox toCrypt;
+        private System.Windows.Forms.TextBox mover;
+        private System.Windows.Forms.Button cipher;
+        private System.Windows.Forms.TextBox encrypted_text;
+        private System.Windows.Forms.TextBox origin_text;
+        private System.Windows.Forms.Button decryptor;
+        private System.Windows.Forms.Label resultAnlasyzis;
+    }
+}
+
---
+++ caesar_cipher/caesar_cipher/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caesar_cipher
+{
+    public partial class Form1 : Form
+    {
+        string alph = "абвгдеёжзийклмнопрстуфхцчшщъьыэюя ";
+        double[] probability = new double[] { 0.062, 0.014, 0.038, 0.013, 0.025, 0.072, 0.072, 
+            0.007, 0.016, 0.062, 0.010, 0.028, 0.035, 0.026, 0.052, 0.090, 0.023, 0.040, 0.045,
+            0.053, 0.021, 0.001, 0.009, 0.004, 0.012, 0.005, 0.003, 0.015, 0.017, 0.015, 0.002,
+            0.006, 0.018, 0.175 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cipher_Click(object sender, EventArgs e)
+        {
+            origin_text.Text = origin_text.Text.ToLower();
+            string crypted = "";
+            if (int.TryParse(mover.Text, out int move))
+            {
+                for (int i = 0; i < origin_text.Text.Length; i++)
+                {
+                    for (int j =0; j < alph.Length; j++)
+                    {
+                        if (origin_text.Text[i] == alph[j])
+                        {
+                            int index = j + move;
+                            if (index < 0) index += alph.Length - 1;
+                            else if (index >= alph.Length) index -= alph.Length - 1;
+                            crypted += alph[index];
+                            break;
+                        }
+                    }
+                }
+                encrypted_text.Text = crypted;
+            }
+        }
+        int compare(double error, double[] sample, double[] analyzing)
+        {
+            for ( int i = 0; i < analyzing.Length; i++) 
+            { 
+                if (analyzing[i] <= sample[12] + error && analyzing[i] >= sample[12] - error && 
+                    analyzing[i+1]  <= sample[13] + error && analyzing[i+1] >= sample[13] - error &&
+                    analyzing[i+2] <= sample[14] + error && analyzing[i+2] >= sample[14] - error) return i - 2;
+            }
+            return -1;
+        }
+        double[] analyze(string text)
+        {
+            double[] result = new double[alph.Length];
+            for(int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < alph.Length; j++) if (alph[j] == text[i]) result[j]++;
+            }
+            for (int i = 0; i < result.Length; i++) result[i] /= text.Length;
+            return result;
+        }
+        private void decryptor_Click(object sender, EventArgs e)
+        {
+            double[] analyzis = analyze(encrypted_text.Text);
+            int found_off = compare(0.03, probability, analyzis);
+            resultAnlasyzis.Text = Convert.ToString(found_off);
+        }
+    }
+}
