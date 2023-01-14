f0242c0e8c654aee5625d78dd402bfcc66716721
joxerx <n.lebedinsky@bk.ru>
2023-01-14 08:38:34 +0300

    Add files via upload

---
+++ XOR_cipher/XOR_cipher/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace XOR_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainBox = new System.Windows.Forms.GroupBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.visualMode = new System.Windows.Forms.ComboBox();
+            this.workMode = new System.Windows.Forms.ComboBox();
+            this.LFSRmode = new System.Windows.Forms.ComboBox();
+            this.startValue = new System.Windows.Forms.TextBox();
+            this.toCrypt = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.mainBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainBox
+            // 
+            this.mainBox.Controls.Add(this.keyBox);
+            this.mainBox.Controls.Add(this.toCrypt);
+            this.mainBox.Controls.Add(this.startValue);
+            this.mainBox.Controls.Add(this.LFSRmode);
+            this.mainBox.Controls.Add(this.workMode);
+            this.mainBox.Controls.Add(this.visualMode);
+            this.mainBox.Controls.Add(this.outputBox);
+            this.mainBox.Controls.Add(this.inputBox);
+            this.mainBox.Location = new System.Drawing.Point(12, 3);
+            this.mainBox.Name = "mainBox";
+            this.mainBox.Size = new System.Drawing.Size(744, 425);
+            this.mainBox.TabIndex = 0;
+            this.mainBox.TabStop = false;
+            this.mainBox.Text = "Шифрование и дешифровка";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(7, 20);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(731, 128);
+            this.inputBox.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(7, 264);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(731, 155);
+            this.outputBox.TabIndex = 1;
+            // 
+            // visualMode
+            // 
+            this.visualMode.AutoCompleteCustomSource.AddRange(new string[] {
+            "Символьный вид",
+            "Двоичный вид",
+            "Шестнадцатиричный вид"});
+            this.visualMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.visualMode.FormattingEnabled = true;
+            this.visualMode.Items.AddRange(new object[] {
+            "Символьный вид",
+            "Двоичный вид",
+            "Шестнадцатиричный вид"});
+            this.visualMode.Location = new System.Drawing.Point(7, 155);
+            this.visualMode.Name = "visualMode";
+            this.visualMode.Size = new System.Drawing.Size(151, 21);
+            this.visualMode.TabIndex = 2;
+            this.visualMode.SelectedIndexChanged += new System.EventHandler(this.visualMode_SelectedIndexChanged);
+            // 
+            // workMode
+            // 
+            this.workMode.AutoCompleteCustomSource.AddRange(new string[] {
+            "Символьный вид",
+            "Двоичный вид",
+            "Шестнадцатиричный вид"});
+            this.workMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workMode.FormattingEnabled = true;
+            this.workMode.Items.AddRange(new object[] {
+            "Гаммирование",
+            "Скремблер"});
+            this.workMode.Location = new System.Drawing.Point(164, 154);
+            this.workMode.Name = "workMode";
+            this.workMode.Size = new System.Drawing.Size(151, 21);
+            this.workMode.TabIndex = 3;
+            this.workMode.SelectedIndexChanged += new System.EventHandler(this.workMode_SelectedIndexChanged);
+            // 
+            // LFSRmode
+            // 
+            this.LFSRmode.AutoCompleteCustomSource.AddRange(new string[] {
+            "Символьный вид",
+            "Двоичный вид",
+            "Шестнадцатиричный вид"});
+            this.LFSRmode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LFSRmode.FormattingEnabled = true;
+            this.LFSRmode.Items.AddRange(new object[] {
+            "x^11 + x^5 + x^2 + 1",
+            "x^11 + x^2 + 1"});
+            this.LFSRmode.Location = new System.Drawing.Point(321, 154);
+            this.LFSRmode.Name = "LFSRmode";
+            this.LFSRmode.Size = new System.Drawing.Size(151, 21);
+            this.LFSRmode.TabIndex = 4;
+            // 
+            // startValue
+            // 
+            this.startValue.Location = new System.Drawing.Point(479, 154);
+            this.startValue.Name = "startValue";
+            this.startValue.Size = new System.Drawing.Size(100, 20);
+            this.startValue.TabIndex = 5;
+            // 
+            // toCrypt
+            // 
+            this.toCrypt.Location = new System.Drawing.Point(586, 154);
+            this.toCrypt.Name = "toCrypt";
+            this.toCrypt.Size = new System.Drawing.Size(152, 23);
+            this.toCrypt.TabIndex = 6;
+            this.toCrypt.Text = "Применить";
+            this.toCrypt.UseVisualStyleBackColor = true;
+            this.toCrypt.Click += new System.EventHandler(this.toCrypt_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(7, 183);
+            this.keyBox.Multiline = true;
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(731, 75);
+            this.keyBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 448);
+            this.Controls.Add(this.mainBox);
+            this.Name = "Form1";
+            this.Text = "Гаммирование";
+            this.mainBox.ResumeLayout(false);
+            this.mainBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mainBox;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.ComboBox visualMode;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.ComboBox LFSRmode;
+        private System.Windows.Forms.ComboBox workMode;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button toCrypt;
+        private System.Windows.Forms.TextBox startValue;
+    }
+}
+
---
+++ XOR_cipher/XOR_cipher/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.Win32;
+
+namespace XOR_cipher
+{
+    public partial class Form1 : Form
+    {
+        int shiftRegister = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            visualMode.SelectedIndex = 0;
+            workMode.SelectedIndex = 0;
+            LFSRmode.SelectedIndex = 0;
+        }
+
+        private void visualMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string input = inputBox.Text;
+            string output = outputBox.Text;
+            string key = keyBox.Text;
+
+            if (visualMode.SelectedIndex == 0)
+            {
+                inputBox.Text = input;
+                outputBox.Text = output;
+                keyBox.Text = key;
+            }
+            else if (visualMode.SelectedIndex == 1) 
+            {
+                inputBox.Text = "";
+                outputBox.Text = "";
+                keyBox.Text = "";
+                foreach (char c in input)
+                    inputBox.Text += Convert.ToString(c, 2).PadLeft(8, '0') + "-";
+                foreach (char c in key)
+                    keyBox.Text += Convert.ToString(c, 2).PadLeft(8, '0') + "-";
+                foreach (char c in output)
+                    outputBox.Text += Convert.ToString(c, 2).PadLeft(8, '0') + "-";
+            }
+            else if (visualMode.SelectedIndex == 2)
+            {
+                inputBox.Text = BitConverter.ToString(Encoding.Default.GetBytes(input));
+                outputBox.Text = BitConverter.ToString(Encoding.Default.GetBytes(output));
+                keyBox.Text = BitConverter.ToString(Encoding.Default.GetBytes(key));
+            }
+
+        }
+
+        private void workMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(workMode.SelectedIndex == 0)
+            {
+                LFSRmode.Enabled = false;
+                startValue.Enabled = false;
+            }
+            else
+            {
+                LFSRmode.Enabled = true;
+                startValue.Enabled = true;
+            }
+        }
+
+        private void toCrypt_Click(object sender, EventArgs e)
+        {
+            string key = keyBox.Text;
+            string input = inputBox.Text;
+            if (workMode.SelectedIndex == 1 && int.TryParse(startValue.Text, out int register)) shiftRegister = register;
+            if (key.Length == 0 || key.Length != input.Length ) 
+            {
+
+                Random rnd = new Random();
+                byte[] keyBytes = new byte[input.Length];
+                
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (workMode.SelectedIndex == 0) keyBytes[i] = Convert.ToByte(rnd.Next(256));
+                    else 
+                    {                        
+                        string binKey = "";
+                        for (int j = 0; j < 8; j++) binKey += LFSR();
+                        keyBytes[i] = Convert.ToByte(binKey, 2);
+                    }
+                }
+                
+                keyBox.Text = System.Text.Encoding.Default.GetString(keyBytes);
+                StreamWriter sw = new StreamWriter("key.txt", false, Encoding.Default);
+                sw.Write(keyBox.Text);
+                sw.Close();
+            }
+            if (keyBox.Text.Length == inputBox.Text.Length)
+            {
+                StreamWriter swo = new StreamWriter("input.txt", false, Encoding.Default);
+                swo.Write(input);
+                swo.Close();
+                byte[] inputText = Encoding.Default.GetBytes(inputBox.Text);
+                byte[] keyText = Encoding.Default.GetBytes(keyBox.Text);
+                byte[] outputText = new byte[inputText.Length];
+                for (int i = 0; i < inputBox.Text.Length; i++) outputText[i] = Convert.ToByte(inputText[i] ^ keyText[i]);
+                outputBox.Text = System.Text.Encoding.Default.GetString(outputText);
+                StreamWriter sw = new StreamWriter("output.txt", false, Encoding.Default);
+                sw.Write(outputBox.Text);
+                sw.Close();
+            }
+        }
+        int LFSR()
+        {
+            if (LFSRmode.SelectedIndex == 0) shiftRegister = ((((shiftRegister >> 5) ^ (shiftRegister >> 2) ^ shiftRegister) & 0x01) << 10) | (shiftRegister >> 1);
+            else shiftRegister = ((((shiftRegister >> 2) ^ shiftRegister) & 0x01) << 10 | shiftRegister >> 1);
+            return shiftRegister & 0x01;
+        }
+    }
+}
